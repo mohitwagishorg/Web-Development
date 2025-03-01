9ace7bb5df10004bcb799bfa6476ab67faf5f726
Mohit <2021pcs5380@mnit.ac.in>
2025-03-01 17:06:49 +0530

    Number data type

--- Js learn/Basic.js
+++ Js learn/Basic.js
@@ -1,4 +1,22 @@
 console.log("Hello world");
+// this will load in browser console
 // ctrl + ~ to open terminal
 // node filename - to run js file
+//npm i -g prettier
 
+//Variables - box
+let favroitenumber = 3;
+favroitenumber = 7; // let can be reassigned
+console.log(favroitenumber); //shows that it runs from top to bottom
+
+let a = 2;
+let g = 8;
+let d = a;
+d = 767;
+console.log(a);
+console.log(d);
+console.log(a); // shows d will make a copy of a ,this is because it premitive datatypes stores in stack memory, hence do not change original value
+
+let z = 9.8;
+console.log(typeof a);
+console.log(typeof z);
